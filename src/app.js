1104f6d9179d0ac7498ef669078e43d7f417807b
NATHAPON <nathapondev1@gmail.com>
2020-08-10 14:01:25 +0700

    First attempt pushing to Github page, CHECKED Responsive and Nav bar, no content yet

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const expansion = document.querySelector(".expansion");
+const toggle = document.querySelector("#toggle");
+
+console.log("hi")
+
+toggle.addEventListener("click", () => {
+    if (toggle.checked === true) {
+        expansion.classList.add("active");
+    }else {
+        expansion.classList.remove("active");
+    }
+});
+
+
+// function check() {
+//     const toggle = document.querySelector("#toggle").checked = true;
+// }
+
+// check()
